e6cea0b3949ae7a99512303ea138116db807f3d5
Hany Shaalan <hshaalan@gmail.com>
2021-03-24 18:20:12 -0400

    added file structure including index.html,assets folder containing css, images and js folders, including script.js and style.css. Also added skeleton html to index.html and imported jquery, moment,google fonts, fonts awesome, and commented out bootstrap since we can't use

---
+++ assets/js/script.js
@@ -0,0 +1,13 @@
+function getApi() {
+    let requestUrl = "https://api.teleport.org/api/cities/?search=Fairfax&limit=10"
+    fetch(requestUrl) 
+      .then(function (response) {
+          return response.json();
+      }
+      )
+      .then(function (data) {
+        console.log(data);
+
+      }) }
+
+getApi()
