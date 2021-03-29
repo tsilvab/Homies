41c67bfb06f5365a6fb6ac451a8a3eb6d17d3370
Hany Shaalan <hshaalan@gmail.com>
2021-03-29 19:25:16 -0400

    improved rendering of jobs data to table but can't make urls clickable

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,9 +8,11 @@ const searchBox = $('#search-now');
 let teleportUrl = "https://api.teleport.org/api/urban_areas/slug:";
 let usajobsUrl = "https://data.usajobs.gov/api/search?Keyword=";
 let outputLocation2 = "";
+let jobsUrl = "";
 
 
 
+// jquery function to auto complete the Location field with a predefined list of 25 locations
 $(function () {
 
     let majorCities = [
@@ -47,12 +49,7 @@ $(function () {
 })
 
 
-
-// var handleFormSubmit = function (event) {
-//     event.preventDefault();
-
-//function to capture both keyword and location search inputs and then construct two urls one for each api and run those get api functions in sub functions
-
+//function to capture both keyword and location search inputs and then construct two urls one for each api and later run those get api functions in sub functions
 function formSubmitHandler (event) {
 event.preventDefault();
 let outputKeyword = inputKeyword.value.trim();
@@ -77,13 +74,14 @@ else {
     outputLocation2 = outputLocation.substring(0, outputLocation.indexOf(',')).replace(/ /,"-").replace(/ /,"-");
 }
 
+// //construct URL for Teleport API
 let requestUrl2 = (teleportUrl + outputLocation2 + "/scores/");
 
 //call 2 x api functions
 getApi1()
 getApi2()
 
-    // USA Jobs API
+    // USA Jobs API function
     function getApi1() {
         fetch(requestUrl1, {
             headers: {
@@ -98,28 +96,34 @@ getApi2()
             .then(function (data) {
                 console.log(data);
 
-            //testing NEW for loop
-            
-            let searchResults = data.SearchResult.SearchResultItems
-            console.log(`lenght of search results is ${searchResults.length}`)
+            //For Loop to capture job title, agency and url          
+            let searchResults = data.SearchResult.SearchResultItems;
             for (let i = 0; i < searchResults.length; i++) {
-                let jobsTitle = searchResults[i].MatchedObjectDescriptor.PositionTitle
-                console.log(`job title is ${jobsTitle}`)
-                let jobsAgency = searchResults[i].MatchedObjectDescriptor.OrganizationName
-                console.log(`job agency is ${jobsAgency}`)
-                let jobsUrl = searchResults[i].MatchedObjectDescriptor.PositionURI
-                console.log(`job url is ${jobsUrl}`)
-            }
+                let jobsTitle = searchResults[i].MatchedObjectDescriptor.PositionTitle;
+                let jobsAgency = searchResults[i].MatchedObjectDescriptor.OrganizationName;
+                jobsUrl = searchResults[i].MatchedObjectDescriptor.PositionURI;
 
-        //capture sample search result from index 0 and paint first table row with title, agency and url. Works great except for URL which needs more work
-        let jobTitle = (data.SearchResult.SearchResultItems[0].MatchedObjectDescriptor.PositionTitle);
-        let jobAgency = (data.SearchResult.SearchResultItems[0].MatchedObjectDescriptor.OrganizationName);
-        let jobLink = (data.SearchResult.SearchResultItems[0].MatchedObjectDescriptor.PositionURI)
-        //*******************use jquery to format link as url. UNSUCCESSFUL//*******************
-        $('#job-table').append('<tr><td>'+jobTitle+'</td><td>'+jobAgency+'</td><td>'+jobLink+'</td></tr>');
-        $('.label primary').append(jobTitle);
-        $('.job-agency').append(jobAgency);
-        $('.job-link').append(jobLink);
+            //Render job title, agency to job search results table. Render url in next function so we build a friendly url
+            $('#job-table').append('<tr><td>'+jobsTitle+'</td><td>'+jobsAgency+'</td><td class="link-td">'+jobsUrl+'</td></tr>');
+           
+            }
+            // --------------TRYING TO SET FRIENDLY URL for jobsUrl----------------
+            //manged to create a foundation css button but unable to properly assign value to each
+            function setUrl () {
+                let link = $('<button type="button" id="view-job" class="success button" <a href="">View Job</a></button>');
+                link.attr("value", jobsUrl)
+                link.attr("href", jobsUrl)
+                // link.attr("href", jobsUrl)
+                // link.attr("text", "View Job")
+                $('.link-td').append(link);
+                $('#view-job').on('click', function () {
+                    let buttonValue = $(this).val();
+                    console.log(buttonValue);
+                })
+                // setTd.$(this)
+            }
+            setUrl ()
+
             })
     }
    
@@ -142,12 +146,3 @@ getApi2()
 //fire search on click. Consider changing to submit to allow searching with Enter or click
 $(searchBox).on('click', formSubmitHandler)
 
-//proof of concept to populate job search table with data using jquery. Next step is to pull data from jobs api and use forloop with function below similar to weather project
-function appendTable () {
-    let jobTitle = "Software Engineer";
-    let jobAgency = "FDIC";
-    let jobLink = "Click me"
-    $('#job-table').append('<tr><td>'+jobTitle+'</td><td>'+jobAgency+'</td><td>'+jobLink+'</td></tr>');
-}
-appendTable()
-// markup = "<tr><td> + information + </td></tr>"
